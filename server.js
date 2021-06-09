3df498a83bc42d77cb83ce4b89053b1ad9dcb671
Jethet <henriette.hettinga@protonmail.com>
2021-06-09 12:50:46 +0200

    Add endpoints for all messages and message by id; create id generator

--- server.js
+++ server.js
@@ -4,22 +4,68 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
+const messages = [
+  {id: 0, from: "Bart", text: "Welcome to CYF chat system!" },
+  { from: "Anne", text: "Good to see you" },
+  { from: "Helen", text: "Long time, no see"},
+  { from: "Anne", text: "let's go for a coffee"}
+];
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+function getNextId() {
+  const lastMessage = messages[messages.length - 1].id  
+  const nextId = parseInt(lastMessage) + 1;
+  return nextId.toString();
+}
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.get("/messages/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+
+  const message = messages.find((message) => message.id === messageId);
+  if (message) {
+    res.send(message);
+  } else {
+    console.log("This message does not exist");
+    res.status(404).send();
+  }
+});
+
+app.post("/messages", (req, res) => {
+  const { from, text } = req.body;
+  const id = getNextId();
+  const newMessage = { id: id, from: from, text: text };
+  if (from === "" || text === "") {
+    console.log("This message is not complete.");
+    res.status(404).send();
+  } else {
+    messages.push(newMessage);
+    console.log("Posted new message", newMessage);
+  }
+});
+
+app.delete("/messages/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+
+  const message = messages.find((message) => message.id === messageId);
+  if (message) {
+    const index = messages.findIndex((message) => message.messageId == messageId);
+    messages.splice(index, 1);
+    res.send({ success: true });
+  } else {
+    console.log("This message does not exist");
+    res.status(404).send();
+  }
+});
+
 app.listen(3000, () => {
-   console.log("Listening on port 3000")
-  });
+  console.log("Listening on port 3000");
+});
