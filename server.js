7700392c0ba6a39f6af03383930cb5567b62c968
Jethet <henriette.hettinga@protonmail.com>
2021-06-09 14:59:13 +0200

    Add put and delete endpoints and test

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ app.use(cors());
 app.use(express.json());
 
 const messages = [
-  {id: 0, from: "Bart", text: "Welcome to CYF chat system!" },
-  { from: "Anne", text: "Good to see you" },
-  { from: "Helen", text: "Long time, no see"},
-  { from: "Anne", text: "let's go for a coffee"}
+  { id: 0, from: "Bart", text: "Welcome to CYF chat system!" },
+  { id: 1, from: "Anne", text: "Good to see you" },
+  { id: 2, from: "Helen", text: "Long time, no see" },
+  { id: 3, from: "Anne", text: "let's go for a coffee" },
 ];
 
 function getNextId() {
-  const lastMessage = messages[messages.length - 1].id  
+  const lastMessage = messages[messages.length - 1].id;
   const nextId = parseInt(lastMessage) + 1;
   return nextId.toString();
 }
@@ -24,18 +24,16 @@ app.get("/", function (request, response) {
 });
 
 app.get("/messages", (req, res) => {
-  res.send(messages);
+  res.status(201).send(messages);
 });
 
 app.get("/messages/:id", (req, res) => {
   const messageId = parseInt(req.params.id);
-
   const message = messages.find((message) => message.id === messageId);
   if (message) {
-    res.send(message);
+    res.status(201).send(message);
   } else {
-    console.log("This message does not exist");
-    res.status(404).send();
+    res.status(404).send("This message does not exist");
   }
 });
 
@@ -44,11 +42,25 @@ app.post("/messages", (req, res) => {
   const id = getNextId();
   const newMessage = { id: id, from: from, text: text };
   if (from === "" || text === "") {
-    console.log("This message is not complete.");
-    res.status(404).send();
+    res.status(404).send("This message is not complete.");
   } else {
     messages.push(newMessage);
-    console.log("Posted new message", newMessage);
+    res.status(201).send(newMessage);
+  }
+});
+
+app.put("/messages/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+  let updatedMessage = req.body;
+  console.log(updatedMessage);
+
+  let message = messages.find((message) => message.id === messageId);
+  if (message) {
+    message.from = updatedMessage.from;
+    message.text = updatedMessage.text;
+    res.status(201).send(updatedMessage);
+  } else {
+    res.status(404).send("This message does not exist");
   }
 });
 
@@ -59,10 +71,9 @@ app.delete("/messages/:id", (req, res) => {
   if (message) {
     const index = messages.findIndex((message) => message.messageId == messageId);
     messages.splice(index, 1);
-    res.send({ success: true });
+    res.status(201).send({ success: true });
   } else {
-    console.log("This message does not exist");
-    res.status(404).send();
+    res.status(404).send("This message does not exist");
   }
 });
 
